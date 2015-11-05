5cb57813ab17d212b213e7a1c9876b1bb0c4b606
Joseph C Wang <joequant@gmail.com>
2015-11-05 16:26:43 +0800

    add cell fixes

--- ethercalc/__init__.py
+++ ethercalc/__init__.py
@@ -1,5 +1,35 @@
 import requests
 
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+         pass
+    try:
+        import unicodedata
+        unicodedata.numeric(s)
+        return True
+    except (TypeError, ValueError):
+        pass
+    return False
+
+def set(coord, item):
+    if isinstance(item, float):
+        return ("set %s value n %f" % (coord, item))
+    elif isinstance(item, int):
+        return ("set %s value n %d" % (coord, item))
+    elif isinstance(item, str) or isinstance(item, unicode):
+        if item == "":
+            return ("set %s empty" % coord)
+        elif item[0] == "'":
+            return ("set %s text t %s" % (coord, item[1:]))
+        elif item[0] == "=":
+            return ("set %s formula %s" % (coord, item[1:]))
+        elif is_number(item):
+            return("set %s value n %s" % (coord, item[1:]))
+        return("set %s text t %s" %item)
+
 class EtherCalc(object):
     def __init__(self, url_root):
         self.root = url_root
@@ -12,6 +42,11 @@ class EtherCalc(object):
         if coord != None:
             api = api + "/" + coord
         return self.get(api)
+    def command(self, page, command):
+        r = requests.post(self.root + "/_/%s" % page,
+                          json = {"command" : command})
+        r.raise_for_status()
+        return r.json()
     def export(self, page, format="json"):
         if format == "json":
             return self.get("/" + page + "/csv.json")
---
+++ examples/hello.py
@@ -0,0 +1,11 @@
+import ethercalc
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+e = ethercalc.EtherCalc("http://localhost:8000")
+e.command("test", ["set C1 value n 2"])
+pp.pprint(e.cells("test"))
+pp.pprint(e.cells("test", "A1"))
+e.command("test", [ ethercalc.set("C1", 2),
+                    ethercalc.set("C2", 2),
+                    ethercalc.set("C3", "=C1+C2") ])
+pp.pprint(e.export("test"))
